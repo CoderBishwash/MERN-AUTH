c2500f506673876231df797d79b9319203341af1
CoderBishwash <intelligentniqqa@gmail.com>
2026-05-16 19:00:44 +0545

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// middleware to use environment variables
+require("dotenv").config();
+
+// connecting to DB
+require("./models/db");
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/ping", (req, res) => {
+  res.send("PONG");
+});
+
+// middleware to take the request from the user
+app.use(bodyParser.json());
+
+// middleware to allow requests from other ports
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT: ${PORT}`);
+});
